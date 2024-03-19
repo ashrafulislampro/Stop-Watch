bf68145a858c6d0722950fa275def276aac85dd2
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-03-19 23:21:15 +0600

    stop watch project done

--- index.c
+++ index.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
+#define CYCLE 5
+
+#ifdef _WIN32
+#include <conio.h>
+#define sleep(x) Sleep(x * 1000)
+#else
+#define clrscr() printf("\e[1;1H\e[2J")
+#include <unistd.h>
+#endif
 
 int main()
 {
     int hour, minute, second;
     printf("Enter Hour Minute Second : ");
     scanf("%d%d%d", &hour, &minute, &second);
-
-    int h, m, s;
+    clrscr();
+    int h = 0, m = 0, s = 0;
     while (1)
     {
-        printf("%0.2d:%0.2d:%0.2d", hour, minute, second);
+        printf("\n\n############# STOP WATCH #############\n\n\n");
+        printf("            %.2d:%.2d:%.2d\n", h, m, s);
+        printf("\n\n############# STOP WATCH #############\n\n\n");
         if (h == hour && m == minute && s == second)
         {
             break;
+        }
+        else
+        {
+            clrscr();
         };
         s++;
-        if (s == 60)
+        sleep(1);
+        if (s == CYCLE)
         {
             m++;
             s = 0;
         };
-        if (m == 60)
+        if (m == CYCLE)
         {
             h++;
             m = 0;
